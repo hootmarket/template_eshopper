918f4de203dfa626a819e96df0a06c84d6cb03bd
Carlos Forero <ecarlosforero@gmail.com>
2016-03-18 13:51:23 -0500

    Initial commit

---
+++ erb/asset/site.js
@@ -0,0 +1,17 @@
+$(function() {
+  var setZoom = function() {
+    $('.s-product-detail-image').magnificPopup({
+      delegate: 'a',
+      type: 'iframe'
+    });
+  };
+  setZoom();
+  
+  $('.h-other-images a').click(function(event) {
+    event.preventDefault();
+    $('.s-product-detail-image a').attr('href', $(this).data('image-zoom'));
+    $('.s-product-detail-image img').css('background-image', 
+      'url(' + $(this).data('image') + ')');
+    setZoom();
+  });
+});
