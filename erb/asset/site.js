6cbf3a935cb81fa632c0f5400657e5f86647f62b
Zdena Forero <zdenaforero@gmail.com>
2016-07-01 20:08:52 -0500

    Updated latest erb code

--- erb/asset/site.js
+++ erb/asset/site.js
@@ -6,11 +6,11 @@ $(function() {
     });
   };
   setZoom();
-  
+
   $('.h-other-images a').click(function(event) {
     event.preventDefault();
     $('.s-product-detail-image a').attr('href', $(this).data('image-zoom'));
-    $('.s-product-detail-image img').css('background-image', 
+    $('.s-product-detail-image img').css('background-image',
       'url(' + $(this).data('image') + ')');
     setZoom();
   });
